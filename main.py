2006abb5b6bca4986a212a15f0f89a39958fb260
kfougere <kfougere@francoisdesales.be>
2021-11-30 20:25:02 +0100

    Add SQL base; Start connection.

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
-import tkinter as tk # Importe le module tkinter
+import tkinter as tk  # Importe le module tkinter
+import sqlite3
 
-class Application(tk.Frame): # objet
-    def __init__(self, kk): #
-        super().__init__(kk)
+class Database():
+    def __init__(self):
+        self.con = sqlite3.connect('equation.db')
+        self.cursor = self.con.cursor()
+        
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS equations (date TEXT, equations TEXT)''')
+
+    def insertInto(self, date, equation):
+
+        self.cursor.execute("INSERT INTO equations (?, ?)", (date,  equation))
+
+
+class Application(tk.Frame):  # objet
+    def __init__(self, master):  #
+        Database()
+
+        super().__init__(master)
         self.grid()
-        self.master = kk
+        self.master = master
+
+        self.master.geometry("350x150")
 
         self.Title = tk.Label(self.master, text="Bienvenue dans imprimeFonction", anchor="w")
-        self.Title.grid(column=0, row=0)
+        self.Title.grid(padx=0, pady=0)
 
         self.eqTxt = tk.Label(self.master, text="Equations :", anchor="w")
-        self.eqTxt.grid(column=0, row=1)
+        self.eqTxt.grid(padx=85, pady=10)
 
         self.entryEq = tk.Entry(self.master)
-        self.entryEq.grid(column=0, row=2)
+        self.entryEq.grid(padx=85, pady=0)
+
+        self.confirmBtn = tk.Button(self.master, text="Valider")
+        self.confirmBtn.grid(padx=0, pady=5)
 
         print(self.entryEq.get())
-
-        print(self.Title)
-
-
-
-
 
 
 root = tk.Tk()
 myApp = Application(root)
 myApp.mainloop()
-
